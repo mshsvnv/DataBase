7dcbebc9103812ccf3bb9e124036e587ba2c4252
mshsvnv <mshsvnv@mail.ru>
2023-09-20 02:52:14 +0300

    started first lab

---
+++ lab_01/src/generator.py
@@ -0,0 +1,159 @@
+from faker import Faker
+import csv
+import random as r
+from datetime import datetime
+
+fake = Faker("ru_RU")
+
+addressesNum = 1000
+schoolsNum = addressesNum
+schools = [i for i in range(1, schoolsNum + 1)]
+
+teachersNum = schoolsNum * 5
+
+studentsNum = 0
+classesNum = 0
+
+subjects = ["Математика", "Русский язык", "Литература", 
+            "Физика", "Химия", "Биология", "География", 
+            "История", "Обществознание", "Английский язык", 
+            "Немецкий язык", "Французский язык", "Испанский язык", 
+            "Музыка", "Изобразительное искусство", 
+            "Физическая культура", "Технология", "Информатика", "ОБЖ"] 
+
+def generateNumber():
+
+    number = "8"
+    for i in range(10):
+        number += str(r.randint(0, 9))
+
+    return number
+
+# addresses
+
+with open("./data/addresses.csv", mode = 'w') as file:
+    writer = csv.writer(file, delimiter=',', lineterminator='\n')
+    
+    writer.writerow(("town", "street", "house"))
+
+    for _ in range(addressesNum):
+        address = fake.address().split(",")
+        
+        town = address[0]
+        street = address[1]
+        house = r.randint(1, 400)
+    
+        writer.writerow((town, street, house))
+
+# schools
+
+addresses = [i + 1 for i in range(addressesNum)]
+
+with open("./data/schools.csv", mode = 'w') as file:
+
+    writer = csv.writer(file, delimiter=',', lineterminator='\n')
+    
+    writer.writerow(("id_address", "construct_year", "type"))
+
+    types = ["Лицей", "Гимназия", ""]
+
+    for i in range(schoolsNum):
+
+        address = r.choice(addresses)
+        addresses.remove(address)
+        
+        year = r.randint(1850, 2023)
+        typeS = r.choice(types)
+
+        writer.writerow((address, year, typeS))
+
+# teachers
+
+with open("./data/teachers.csv", mode = 'w') as file:
+
+    writer = csv.writer(file, delimiter=',', lineterminator='\n')
+    
+    writer.writerow(("full_name", "age", "gender"))
+
+    for i in range(teachersNum):
+        
+        id_school = r.randint(1, schoolsNum + 1)
+
+        name = fake.name()
+        gender = "М" if name[-1] != "а" else "Ж"
+
+        age = r.randint(25, 65)
+
+        writer.writerow((name, age, gender))
+
+
+# classes
+
+with open("./data/classes.csv", mode = 'w') as file:
+    writer = csv.writer(file, delimiter=',', lineterminator='\n')
+    
+    writer.writerow(("id_school", "grade", "letter"))
+
+    for i in range(schoolsNum):
+
+        school = r.choice(schools)
+        schools.remove(school)
+
+        for j in range(11):
+
+            amount = r.randint(1, 3)
+            classesNum += amount
+
+            for k in range(amount):
+                studentsNum += r.randint(10, 15)
+
+                writer.writerow((school, j + 1, str(chr(k + 64))))
+
+# puples
+
+with open("./data/puples.csv", mode = 'w') as file:
+
+    writer = csv.writer(file, delimiter=',', lineterminator='\n')
+
+    writer.writerow(("id_class", "full_name", "gender", "phone"))
+
+    for i in range(studentsNum):
+        id_class = r.randint(1, classesNum + 1)
+
+        name = fake.name()
+        gender = "М" if name[-1] != "а" else "Ж"
+        phone = generateNumber()
+
+        writer.writerow((id_class, name, gender, phone))
+
+pairs = []
+
+with open("./data/lessons.csv", mode = 'w') as file:
+
+    writer = csv.writer(file, delimiter=',', lineterminator='\n')
+
+    writer.writerow(("id_teacher", "id_class", "title",))
+
+    for i in range(classesNum):
+
+        amount = r.randint(10, 15)
+
+        for i in range(amount):
+
+            pair = (r.randint(1, teachersNum),
+                    r.randint(1, classesNum))
+            
+            while (pair in pairs):
+                pair = (r.randint(1, teachersNum),
+                        r.randint(1, classesNum))
+                
+            titile = r.choice(subjects)
+
+            writer.writerow((pair[0], pair[1], titile))
+    
+
+
+            
+
+
+
---
+++ lab_01/src/sql_scripts/constraints.sql
@@ -0,0 +1,48 @@
+ALTER TABLE addresses
+    ADD CONSTRAINT pk_address_id PRIMARY KEY(id),
+
+    ADD CONSTRAINT valid_house CHECK(house > 1),
+
+    ADD COLUMN town SET NOT NULL,
+    ADD COLUMN street SET NOT NULL,
+    ADD COLUMN hous SET NOT NULL;
+
+ALTER TABLE schools
+    ADD CONSTRAINT pk_school_id PRIMARY KEY(id),
+    ADD CONSTRAINT fk_address_id FOREIGN KEY(id_address) REFERENCES addresses(id),
+
+    ALTER COLUMN construct_year SET NOT NULL,
+    ALTER COLUMN id_address SET NOT NULL;
+
+ALTER TABLE classes
+    ADD CONSTRAINT pk_class_id PRIMARY KEY(id),
+    ADD CONSTRAINT fr_school_id FOREIGN KEY(id_school) REFERENCES schools(id),
+    
+    ADD CONSTRAINT valid_grade CHECK(grade > 0 and grade < 12),
+
+    ALTER COLUMN grade SET NOT NULL,
+    ALTER COLUMN letter SET NOT NULL;
+
+ALTER TABLE puples
+    ADD CONSTRAINT pk_puple_id PRIMARY KEY(id),
+    ADD CONSTRAINT fk_class_id FOREIGN KEY(id_class) REFERENCES classes(id),
+
+    ADD CONSTRAINT valid_gender CHECK(gender = "М" or gender = "Ж"),
+
+    ALTER COLUMN full_name SET NOT NULL,
+    ALTER COLUMN phone SET NOT NULL;
+
+ALTER TABLE teachers
+    ADD CONSTRAINT pk_teacher_id PRIMARY KEY(id),
+    
+    ADD CONSTRAINT valid_age CHECK(age > 24 and age < 66),
+    ADD CONSTRAINT valid_gender CHECK(gender = "М" or gender = "Ж"),
+
+    ALTER COLUMN full_name SET NOT NULL,
+    ALTER COLUMN age SET NOT NULL;
+
+ALTER TABLE lessons
+    ADD CONSTRAINT fk_teacher_id FOREIGN KEY(id_teacher) REFERENCES teachers(id),
+    ADD CONSTRAINT fk_class_id FOREIGN KEY(id_class) REFERENCES classes(id),
+
+    ADD CONSTRAINT title SET NOT NULL;
