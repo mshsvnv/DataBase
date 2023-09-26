20bf8bcb81838c75406fb1c5967790357ef5142c
mshsvnv <mshsvnv@mail.ru>
2023-09-26 10:59:49 +0300

    added some requests

--- lab_02/src/main.sql
+++ lab_02/src/main.sql
@@ -28,12 +28,67 @@
 -- ORDER BY title;
 
 -- 5
--- Вывести студентов классов, в которых учится Павел
--- SELECT DISTINCT id_class, id, full_name FROM students S1
--- WHERE EXISTS (SELECT id, id_class, full_name 
---               FROM students S2
---               WHERE S2.full_name LIKE '%Павел%' 
---                     AND S1.id_class = S2.id_class)
+-- Вывесим студентов из A класса
+-- SELECT * FROM students
+-- WHERE EXISTS (SELECT classes.id
+--               FROM classes
+--               WHERE classes.id = students.id_class AND classes.letter = 'A');
+
+-- 6
+-- Вывести самых взрослых преподавателей мужчин
+-- SELECT * FROM teachers
+-- WHERE gender = 'М' AND age >= ALL (SELECT age
+--                                    FROM teachers
+--                                    WHERE gender = 'М');
+
+-- -- 7
+-- Средний возраст преподавателей по гендеру
+-- SELECT gender, AVG(age) AS aver_age
+-- FROM teachers
+-- GROUP BY gender;
+
+-- 8
+-- Вывести учеников и их букву класса
+-- SELECT *, 
+--     (SELECT classes.letter FROM classes
+--     WHERE classes.id = students.id_class) AS class_letter
+-- FROM students
+-- ORDER BY id_class;
+
+-- 9
+-- Направления изучения
+-- SELECT *,
+--     CASE kind
+--         WHEN 'Гимназия'
+--         THEN 'Гуманитарное'
+--         WHEN 'Лицей'
+--         THEN 'Техническое'
+--     END AS direction
+-- from schools
+-- ORDER BY construct_year;
+
+-- 10
+-- Временные промежутки постройки школы
+-- SELECT *,
+--     CASE
+--         WHEN Schools.construct_year < 1918
+--         THEN 'Дореволюционная'
+--         WHEN Schools.construct_year < 1991
+--         THEN 'Советская'
+--         ELSE 'Постсоветская'
+--     END AS status
+-- from schools;
+        
+-- 11
+-- Учеников 100го класса в одну таблицу
+-- SELECT *
+-- INTO class 
+-- FROM students
+-- WHERE students.id_class = 100;
+
+-- DROP TABLE class;
+
+-- 12
 
 -- 16
 -- INSERT INTO teachers (id, full_name, age, gender)
