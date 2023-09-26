dd5bc22d539e6c583d1d6671dcb19f2c41be15ca
mshsvnv <mshsvnv@mail.ru>
2023-09-27 01:13:47 +0300

    left 2 requests(((

--- lab_01/src/sql_scripts/copy.sql
+++ lab_01/src/sql_scripts/copy.sql
@@ -1,35 +1,35 @@
 COPY addresses
-FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\DataBase\lab_01\src\data\addresses.csv'
+FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\Data Base\lab_01\src\data\addresses.csv'
 WITH (format csv,
     header,
     delimiter ";");
 
 COPY teachers
-FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\DataBase\lab_01\src\data\teachers.csv'
+FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\Data Base\lab_01\src\data\teachers.csv'
 WITH (format csv,
     header,
     delimiter ";");
 
 COPY schools
-FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\DataBase\lab_01\src\data\schools.csv'
+FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\Data Base\lab_01\src\data\schools.csv'
 WITH (format csv,
     header,
     delimiter ";");
 
 COPY classes
-FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\DataBase\lab_01\src\data\classes.csv'
+FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\Data Base\lab_01\src\data\classes.csv'
 WITH (format csv,
     header,
     delimiter ";");
 
 COPY lessons
-FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\DataBase\lab_01\src\data\lessons.csv'
+FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\Data Base\lab_01\src\data\lessons.csv'
 WITH (format csv,
     header,
     delimiter ";");
 
 COPY students
-FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\DataBase\lab_01\src\data\students.csv'
+FROM 'C:\Users\mshsv\OneDrive\Рабочий стол\studying\5_sem\Data Base\lab_01\src\data\students.csv'
 WITH (format csv,
     header,
     delimiter ";");
--- lab_02/src/main.sql
+++ lab_02/src/main.sql
@@ -89,11 +89,101 @@
 -- DROP TABLE class;
 
 -- 12
+-- Вывести 10е классы
+-- SELECT *
+-- FROM lessons JOIN (SELECT *
+--                    FROM classes
+--                    WHERE grade = 10) AS C ON C.id = lessons.id_class
+-- ORDER BY title;
+
+-- 13
+-- Вывести предметы в Москве
+-- SELECT *
+-- FROM lessons
+-- WHERE id_class IN (SELECT id
+--                   FROM classes
+--                   WHERE id_school IN (SELECT id
+--                                      FROM schools
+--                                      WHERE id_address IN (SELECT id
+--                                                         FROM addresses
+--                                                         WHERE town LIKE '%Москва%')));
+
+-- 14
+-- SELECT kind, AVG(construct_year) as average
+-- FROM schools
+-- GROUP BY kind;
+
+-- 15
+-- Вывести школы по категориям, ср. зн-ие года постройки меньше общего значения
+-- SELECT kind, AVG(construct_year) as average
+-- FROM schools
+-- GROUP BY kind
+-- HAVING AVG(construct_year) < (SELECT AVG(construct_year)
+--                          FROM schools);
 
 -- 16
 -- INSERT INTO teachers (id, full_name, age, gender)
 -- VALUES (112233, 'Иванов Иван Иванович', 65, 'М');
 
+-- 17
+-- INSERT INTO lessons (id_teacher, id_class, title)
+--     SELECT * FROM lessons
+--     WHERE title = 'Биология' 
+--     AND id_class IN (SELECT id
+--                       FROM classes
+--                       WHERE letter = 'A');
+
+-- 18
+-- Продлим молодость 65 летним на 5 лет
+-- UPDATE teachers
+-- SET age = age - 5
+-- WHERE age = 65;
+
+-- 19
+-- Заменяем для гимназий год постройки на среднее значение
+-- UPDATE schools
+-- SET construct_year = (SELECT AVG(construct_year)
+--                       FROM schools)
+-- WHERE kind = 'Гимназия';
+
 -- 20
--- DELETE addresses
--- WHERE town LIKE '%Москва%';
+-- DELETE teachers
+-- WHERE full_name = 'Иванов Иван Иванович';
+
+-- 21
+-- SELECT * FROM students
+-- WHERE id in (SELECT id
+--              FROM classes 
+--              WHERE id_school = 2);
+
+-- DELETE FROM students
+-- WHERE id IN (SELECT id
+--              FROM classes 
+--              WHERE id_school = 2);
+
+-- SELECT * FROM students
+-- WHERE id in (SELECT id
+--              FROM classes 
+--              WHERE id_school = 2);
+
+-- 22
+-- WITH ST_10 AS
+--     (SELECT * from students
+--     WHERE id_class IN (SELECT id
+--                        FROM classes
+--                        WHERE grade = 10 and letter = 'A'))
+
+-- SELECT * FROM ST_10;
+
+-- 23
+-- TODO
+
+-- 24
+-- SELECT *, 
+--     AVG(construct_year) OVER (PARTITION BY kind) AS aver,
+--     COUNT(*) OVER (PARTITION BY kind) AS count
+-- FROM schools
+-- WHERE construct_year > 2000;
+
+-- 25
+-- TODO
