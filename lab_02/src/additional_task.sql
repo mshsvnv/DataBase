c75ac0fd90ea035e14b6082e03e00fc1bd29bca6
mshsvnv <mshsvnv@mail.ru>
2023-11-10 17:43:07 +0300

    add additional task

--- lab_02/src/additional_task.sql
+++ lab_02/src/additional_task.sql
@@ -1,113 +1,64 @@
-
-CREATE SCHEMA lab_02_extra
-
-CREATE TABLE IF NOT EXISTS lab_02_extra.table1(
-    id int,
-    var1 text,
-    valid_from_dttm date default '5999-12-31',
-    valid_to_dttm date default '5999-12-31'
+-- Active: 1696673577093@@127.0.0.1@5432@task
+CREATE TABLE IF NOT EXISTS table1(
+    id INT,
+    var1 TEXT,
+    valid_from_dttm DATE DEFAULT '5999-12-31',
+    valid_to_dttm DATE DEFAULT '5999-12-31'
 );
 
-CREATE TABLE IF NOT EXISTS lab_02_extra.table2(
-    id int,
-    var2 text,
-    valid_from_dttm date default '5999-12-31',
-    valid_to_dttm date default '5999-12-31'
+CREATE TABLE IF NOT EXISTS table2(
+    id INT,
+    var2 TEXT,
+    valid_from_dttm DATE DEFAULT '5999-12-31',
+    valid_to_dttm DATE DEFAULT '5999-12-31'
 );
 
-insert into lab_02_extra.table1 values(1, 'A', '2018-09-01', '2018-09-15');
-insert into lab_02_extra.table1 values(1, 'B', '2018-09-16', default);
-insert into lab_02_extra.table1 values(2, 'С', '2018-09-01', '2018-09-13');
-insert into lab_02_extra.table1 values(2, 'В', '2018-09-16', default);
-insert into lab_02_extra.table1 values(3, 'T', '2018-09-10', '2018-09-30');
-insert into lab_02_extra.table1 values(4, 'Y', '2018-09-16', '2018-10-01');
+VALUES
+    (1, 'A', '2018-09-01', '2018-09-15'),
+    (1, 'B', '2018-09-16', '5999-12-31'),
+    (3, 'A', '2015-11-11', '2016-11-13');
 
-insert into lab_02_extra.table2 values(1, 'A', '2018-09-01', '2018-09-18');
-insert into lab_02_extra.table2 values(1, 'B', '2018-09-19', default);
-insert into lab_02_extra.table2 values(2, 'E', '2018-09-01', '2018-09-23');
-insert into lab_02_extra.table2 values(2, 'F', '2018-09-24', default);
-insert into lab_02_extra.table2 values(3, 'L', '2018-09-23', '2018-09-30');
-insert into lab_02_extra.table2 values(3, 'L', '2018-10-01', default);
-insert into lab_02_extra.table2 values(4, 'U', '2018-09-30', default);
+INSERT INTO table2(ID, var2, valid_from_dttm, valid_to_dttm)
+VALUES
+    (1, 'A', '2018-09-01', '2018-09-12'),
+    (1, 'A', '2018-09-13', '2018-09-18'),
+    (1, 'B', '2018-09-19', '5999-12-31'),
+    (2, 'C', '2019-11-11', '2019-11-13');
 
-SELECT id, var1, var2, valid_from_dttm, valid_to_dttm
-FROM (
-    SELECT t1.id, t1.var1, t2.var2,
-		GREATEST(t1.valid_from_dttm, t2.valid_from_dttm) AS valid_from_dttm,
-		LEAST(t1.valid_to_dttm, t2.valid_to_dttm) AS valid_to_dttm
---         CASE 
---             WHEN t1.valid_from_dttm > t2.valid_from_dttm THEN t1.valid_from_dttm
---             WHEN t2.valid_from_dttm > t1.valid_from_dttm THEN t2.valid_from_dttm
---             ELSE t1.valid_from_dttm
---         END valid_from_dttm,
---         CASE 
---             WHEN t1.valid_to_dttm < t2.valid_to_dttm THEN t1.valid_to_dttm
---             WHEN t2.valid_to_dttm < t1.valid_to_dttm THEN t2.valid_to_dttm
---             ELSE t1.valid_to_dttm
---         END valid_to_dttm
-    FROM 
-		lab_02_extra.table1 AS t1 JOIN 
-		lab_02_extra.table2 AS t2 ON t1.id = t2.id
-) as dttm
-WHERE valid_from_dttm <= valid_to_dttm
-group by id, var1, var2, valid_from_dttm, valid_to_dttm
-ORDER BY id, valid_from_dttm;
+SELECT * FROM table1;
+SELECT * FROM table2;
 
+DROP TABLE IF EXISTS table1, table2;
 
--- DROP TABLE IF EXISTS  Table_1, Table_2;
---
--- CREATE TABLE IF NOT EXISTS Table_1
--- (
---     id              int,
---     var1            varchar(100),
---     valid_from_dttm date,
---     valid_to_dttm   date
--- );
---
--- CREATE TABLE IF NOT EXISTS Table_2
--- (
---     id              int,
---     var2            varchar(100),
---     valid_from_dttm date,
---     valid_to_dttm   date
--- );
---
--- INSERT INTO table_1(id, var1, valid_from_dttm, valid_to_dttm)
--- VALUES
---     (1, 'A', '2018-09-01', '2018-09-15'),
---     (1, 'B', '2018-09-16', '5999-12-31');
---
--- INSERT INTO table_2(id, var2, valid_from_dttm, valid_to_dttm)
--- VALUES
---     (1, 'A', '2018-09-01', '2018-09-18'),
---     (1, 'B', '2018-09-19', '5999-12-31');
---
--- WITH select_all AS
--- (
---     SELECT *
---     FROM table_1
---
---     UNION ALL
---
---     SELECT *
---     FROM table_2
--- )
--- SELECT *
--- FROM select_all;
---
---
--- WITH ordered_dates AS
--- (
---     SELECT *
---     FROM table_1
---
---     UNION ALL
---
---     SELECT *
---     FROM table_2
--- )
--- SELECT *
--- FROM ordered_dates;
---
--- DROP TABLE IF EXISTS table_1;
--- DROP TABLE IF EXISTS table_2;
+WITH from_dttm AS (
+    SELECT id, valid_from_dttm FROM table1
+    UNION
+    SELECT id, valid_to_dttm FROM table1
+    UNION
+    SELECT id, valid_from_dttm FROM table2
+    UNION
+    SELECT id, valid_to_dttm FROM table2
+),
+to_dttm AS (
+    SELECT id, valid_to_dttm FROM table1
+    UNION
+    SELECT id, valid_from_dttm FROM table1
+    UNION
+    SELECT id, valid_to_dttm FROM table2
+    UNION
+    SELECT id, valid_from_dttm FROM table2
+),
+ranges AS (
+    SELECT fd.id, valid_from_dttm, MIN(valid_to_dttm) AS valid_to_dttm
+    FROM from_dttm fd
+    JOIN to_dttm td ON td.id = fd.id AND td.valid_to_dttm > fd.valid_from_dttm
+    GROUP BY fd.id, valid_from_dttm
+)
+SELECT r.id,
+       t1.var1, t2.var2,
+       r.valid_from_dttm, r.valid_to_dttm
+FROM ranges r
+FULL JOIN table1 t1 ON t1.id = r.id
+FULL JOIN table2 t2 ON t2.id = r.id 
+WHERE r.valid_from_dttm in (t1.valid_from_dttm, t2.valid_from_dttm) AND r.valid_to_dttm in (t1.valid_to_dttm, t2.valid_to_dttm)
+ORDER BY r.id, r.valid_from_dttm;
